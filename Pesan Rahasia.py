649d6c94f2635ca7ac98a736910e1c3ad1a53635
arihidayatm <95893857+arihidayatm@users.noreply.github.com>
2021-12-10 15:27:11 +0700

    Add files via upload

---
+++ Pesan Rahasia.py
@@ -0,0 +1,9 @@
+REGEX_ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+
+
+def cypher(msg):
+    msg = msg.lower()
+    trans_map = msg.maketrans(REGEX_ALPHABET, REGEX_ALPHABET[::-1])
+    return
+    sent = input()
+    print(cypher(sent))
